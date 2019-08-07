788b5711e76206cea0098ee038e33a27649771e5
Diego E. Avila <adiego73@gmail.com>
2019-08-07 03:30:01 +0200

    final

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ const HEIGHT = 600;
 // constants
 const LINE_WIDTH = 5; // line width goes from 1 to 10
 const MIN_BLEND_RADIUS = 10;
-const MIN_LENGTH = 5;
-const MAX_LENGTH = 100;
+const MIN_LENGTH = 60;
+const MAX_LENGTH = 150;
 
 /**
  * Bootstrap the application.
@@ -31,25 +31,6 @@ function bootstrap() {
 }
 
 /**
- * Draws an arc segment
- * @param x coordinate of the arc's center
- * @param y coordinate of the arc's center
- * @param inRadius inner radius of the arc
- * @param outRadius outer radius of the arc
- * @param startAngle starting angle
- * @param endAngle ending angle.
- * @param strokeColor
- * @param fillColor
- * @private
- */
-function __drawSegment(x, y, inRadius, outRadius, startAngle, endAngle, strokeColor = "black", fillColor = "white") {
-    let arc = two.makeArcSegment(x, y, inRadius, outRadius, startAngle, endAngle);
-    arc.stroke = strokeColor;
-    arc.fill = fillColor;
-    arc.linewidth = 1;
-}
-
-/**
  * Draws the bowl.
  * @param x coordinate of the bowl's center
  * @param y coordinate of the bowl's center
@@ -57,15 +38,6 @@ function __drawSegment(x, y, inRadius, outRadius, startAngle, endAngle, strokeCo
  * @param width
  */
 function createBowl(x, y, radius, width = 6) {
-    // let outerCircle = two.makeCircle(x, y, radius + width);
-    // outerCircle.linewidth = 1;
-    // outerCircle.stroke = "black";
-    // outerCircle.noFill();
-    //
-    // let innerCircle = two.makeCircle(x, y, radius);
-    // innerCircle.linewidth = 1;
-    // innerCircle.stroke = "black";
-    // innerCircle.noFill();
     let bowl = two.makeArcSegment(x, y, radius, radius + width, 0, 2 * Math.PI);
     bowl.fill = Utils.randomColor();
     bowl.stroke = "black";
@@ -83,14 +55,16 @@ function createBowl(x, y, radius, width = 6) {
 function createNoodle(Bowl, minLength, maxLength, minBlendRadius, lineWidth) {
     // setup the arc length.
     let length = _.random(minLength, maxLength, true);
-    let radius = _.random(minBlendRadius, 2 * minBlendRadius, true);
+    let radius = _.random(minBlendRadius, 4 * minBlendRadius, true);
 
-    // amount of complete arcs
-    let amountOfArcs = length / (radius * Utils.degToRad(180));
+    // amount arcs based on the length (ex. 1.75)
+    let amountOfArcs = (length / radius) / Utils.degToRad(180);
+    // amount of half-circle arcs (ex. 1)
     let integerAmountOfArcs = Math.floor(amountOfArcs);
+    // less-than-an-arc arcs (ex. 0.75 --> means, 0.75 * 180deg arc)
+    let decimalAmountOfArcs = amountOfArcs % 1;
 
     let arcs = _.map(_.range(integerAmountOfArcs), (i) => {
-        length -= length / integerAmountOfArcs;
         let arc_x = Bowl.x + (2 * i * radius) - (i * lineWidth);
         let arc_y = Bowl.y;
 
@@ -98,29 +72,43 @@ function createNoodle(Bowl, minLength, maxLength, minBlendRadius, lineWidth) {
         return new Two.ArcSegment(arc_x, arc_y, radius - lineWidth, radius, start, Utils.degToRad(0));
     });
 
-    if (length > 0) {
+    if (decimalAmountOfArcs > 0) {
         let arc_x = Bowl.x + (2 * integerAmountOfArcs * radius) - (integerAmountOfArcs * lineWidth);
         let arc_y = Bowl.y;
 
-        let angle = (length / radius) - Utils.degToRad(180);
+        // define the start and angle of the less-than-an-arc arc
+        let angle = (decimalAmountOfArcs * Math.PI - Utils.degToRad(180)) * ((integerAmountOfArcs % 2 === 1) ? -1: 1);
         let start = (integerAmountOfArcs % 2 === 1) ? Utils.degToRad(180) : Utils.degToRad(-180);
-        let end = angle * -1;
-        let arc = new Two.ArcSegment(arc_x, arc_y, radius - lineWidth, radius, start, end);
+        let arc = new Two.ArcSegment(arc_x, arc_y, radius - lineWidth, radius, start, angle);
         arcs.push(arc);
     }
 
+    // get the noodle radius
     let noodleRadius = _.reduce(arcs, (sum, e) => {
-         return sum + (e.innerRadius);
+         return sum + (e.outerRadius);
     }, 0);
 
-    let innerRadius = Bowl.radius - noodleRadius - lineWidth;
+    // the noodle should be inside a circumference defined as the bowl circumference minus the noodle radius.
+    // since the noodle is centered on its centroid, there is no problem to be over the perimeter of the inner circle.
+    let innerRadius = Bowl.radius - noodleRadius;
 
     let rand = _.random(0, innerRadius, true) * 2 * Math.PI;
-    let x = (innerRadius * Math.sqrt(_.random(0, 1, true))) * Math.cos(rand) + Bowl.x;
-    let y = (innerRadius * Math.sqrt(_.random(0, 1, true))) * Math.sin(rand) + Bowl.y;
+    let x, y;
+    // we need to cover all the bowl, so:
+    //  - get a random position inside a circumference
+    // - or get a random position over a circumference
+    if(_.random(0, 10) % 2) {
+        x = (innerRadius * Math.sqrt(_.random(0, 1, true))) * Math.cos(rand) + Bowl.x;
+        y = (innerRadius * Math.sqrt(_.random(0, 1, true))) * Math.sin(rand) + Bowl.y;
+    }else{
+        x = innerRadius * Math.cos(rand) + Bowl.x;
+        y = innerRadius * Math.sin(rand) + Bowl.y;
+    }
 
+    // two.makeCircle(x,y,2).fill = "red";
+
+    // define the noodle as the concatenation of the N arcs.
     let color = "white";
-    // color = Utils.randomColor();
     let group = two.makeGroup(arcs);
     group.center();
     group.translation = new Two.Vector(x, y);
@@ -139,7 +127,7 @@ function main() {
     let radius = ((HEIGHT < WIDTH) ? HEIGHT : WIDTH) / 3;
 
     // amount of noodles to create
-    let qtyNoodles = Utils.map(LINE_WIDTH, 10, 1, 800, 5000);
+    let qtyNoodles = Utils.map(LINE_WIDTH, 10, 1, 800, 3000);
 
     for (let i = qtyNoodles; i > 0; i--)
         createNoodle({
